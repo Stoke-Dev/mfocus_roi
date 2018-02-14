60011d8b6a526496c4a1ef3372b5c04f30054e63
Brandyn Burbank <brandyn@brandynburbank.com>
2018-02-14 15:34:56 -0700

    retooled some of the styles for the calculator page, updated markup and added data structure on the backend for the categories

--- MicroFocus-ROI-app/src/app/calculator/calculator.component.ts
+++ MicroFocus-ROI-app/src/app/calculator/calculator.component.ts
@@ -8,6 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+  categories = [
+    {
+      id: "id_gov",
+      name: "Identity Governance",
+      description: "Lorem Ipsum description of item.",
+      savings: 3500,
+      checked: false
+    },
+    {
+      id: "ac_mgmt",
+      name: "Access Management",
+      description: "Lorem Ipsum description of item.",
+      savings: 3500,
+      checked: false
+    },
+    {
+      id: "prv_mgmt",
+      name: "Privilege Management",
+      description: "Lorem Ipsum description of item.",
+      savings: 3500,
+      checked: false
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
