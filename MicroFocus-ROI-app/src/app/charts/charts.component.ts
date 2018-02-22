d85dbddc36ecb437f9ef5c4a5460a55555afa3a4
Brandyn Burbank <brandyn@brandynburbank.com>
2018-02-21 21:46:10 -0700

    dynamic chart data, and few formatting edits

--- MicroFocus-ROI-app/src/app/charts/charts.component.ts
+++ MicroFocus-ROI-app/src/app/charts/charts.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MfClientDataService } from '../mf-client-data.service';
+import { Component, OnInit, Input } from '@angular/core';
+// import { MfClientDataService } from '../mf-client-data.service';
 
 @Component({
   selector: 'app-charts',
@@ -8,16 +8,43 @@ import { MfClientDataService } from '../mf-client-data.service';
 })
 export class ChartsComponent {
 
-/* The graph is responive, and will automatically set the limits of the X and Y axis. All you have to do it add data */
+  @Input() data: number[];
 
-    // lineChart
-  public lineChartData:Array<any> = [
-    {data: [0, 30000], label: 'With Micro Focus'}, /* Additional data can be added, separated by commas. If you do add data, you HAVE to also add to the lineChartsLabels array */
-    {data: [0, 20000], label: 'Without Micro Focus'},
-  ];
-  public lineChartLabels: Array<any> = ['Year 1', 'Year 5'];
+  ngOnInit() {
+    this.lineChartData = [
+      { data: this.data, label: 'ROI With Micro Focus' }, /* Additional data can be added, separated by commas. If you do add data, you HAVE to also add to the lineChartsLabels array */
+      // {data: [0, 20000], label: 'Without Micro Focus'},
+    ];
+  }
+
+  /* The graph is responive, and will automatically set the limits of the X and Y axis. All you have to do it add data */
+
+  // lineChart
+  public lineChartData: Array<any>;
+  public lineChartLabels: Array<any> = ['Year 1', 'Year 2', 'Year 3', 'Year 4', 'Year 5'];
   public lineChartOptions: any = {
-    responsive: true
+    responsive: true,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          stepSize: 500000,
+
+          // Return an empty string to draw the tick line but hide the tick label
+          // Return `null` or `undefined` to hide the tick line entirely
+          userCallback: function (value, index, values) {
+            // Convert the number to a string and splite the string every 3 charaters from the end
+            value = value.toString();
+            value = value.split(/(?=(?:...)*$)/);
+
+            // Convert the array to a string and format the output
+            value = value.join(',');
+            return '$' + value;
+          }
+        }
+      }]
+    }
+
   };
   public lineChartColors: Array<any> = [
     { // grey
@@ -47,12 +74,12 @@ export class ChartsComponent {
   ];
   public lineChartLegend: boolean = true;
   public lineChartType: string = 'line';
-  
+
   // events
   public chartClicked(e: any): void {
     console.log(e);
   }
-  
+
   public chartHovered(e: any): void {
     console.log(e);
   }
