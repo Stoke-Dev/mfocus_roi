11768876e1a31bd77b3e25fa1410ff6e30eee810
Brandyn Burbank <brandyn@brandynburbank.com>
2018-06-11 13:40:51 -0600

    fix typing and MVC structure issues

--- MicroFocus-ROI-app/src/app/helper-box/helper-box.component.ts
+++ MicroFocus-ROI-app/src/app/helper-box/helper-box.component.ts
@@ -8,6 +8,14 @@ import { TooltipDataService } from '../tooltip-data.service';
 })
 export class HelperBoxComponent implements OnInit {
 
+  get ttLearnMore():number {return this._data.learnMoreUrl;}
+  get ttImgURL():number {return this._data.imageUrl;}
+  get ttShowCaret():boolean {return this._data.showCaret;}
+  get ttCaretOffset():number {return this._data.caretOffset;}
+  get ttTitle():String {return this._data.title;}
+  get ttDescription():String {return this._data.description;}
+
+
   constructor(private _data: TooltipDataService) { }
 
   ngOnInit() {
